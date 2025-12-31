dcdb28b2e81ac6e9cebcc3b86c134ad54b66c934
BhavyankVith <34795451+BhavyankVith@users.noreply.github.com>
2025-12-31 21:39:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,20 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+# This block looks up the latest Amazon Linux 2 AMI in Tokyo automatically
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "app" {
-  ami           = "ami-0b46816ffa1234887"
+  # We reference the data source here instead of a hardcoded string
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
 
   tags = {
