01666b3fc05417b3e7072ba4a7e055d93fb05225
BhavyankVith <34795451+BhavyankVith@users.noreply.github.com>
2025-12-31 21:57:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,15 @@ data "aws_ami" "amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-
+resource "aws_key_pair" "devops_key" {
+  key_name   = "devops-key"
+  public_key = file("~/.ssh/devops-key.pub")
+}
 resource "aws_instance" "app" {
   # We reference the data source here instead of a hardcoded string
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
+  key_name      = aws_key_pair.devops_key.key_name
 
   tags = {
     Name = "devops-demo"
