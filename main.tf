57be9182b86605e8c4623421946ca348cf235de9
BhavyankVith <34795451+BhavyankVith@users.noreply.github.com>
2026-01-01 01:28:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,36 @@ resource "aws_key_pair" "devops_key" {
   key_name   = "devops-key"
   public_key = file("~/.ssh/devops-key.pub")
 }
+resource "aws_security_group" "web_sg" {
+  name        = "allow_web_ssh"
+  description = "Allow HTTP and SSH"
+
+  # 1. SSH Access (Missing this was causing the timeout)
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  # 2. HTTP Access (For the browser)
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # 3. Outbound Rules (Allow instance to talk to the internet)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "app" {
   # We reference the data source here instead of a hardcoded string
   ami           = data.aws_ami.amazon_linux.id
